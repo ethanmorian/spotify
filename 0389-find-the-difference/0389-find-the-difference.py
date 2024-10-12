08f12af1aeb390d739487d324b0a8a2e3faf3aa8
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-10-12 20:05:23 +0900

    Time: 34 ms (78.77%), Space: 16.6 MB (66.42%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,11 +1,6 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         result = 0
-        
-        for char in s:
+        for char in s + t:
             result ^= ord(char)
-            
-        for char in t:
-            result ^= ord(char)
-        
         return chr(result)
