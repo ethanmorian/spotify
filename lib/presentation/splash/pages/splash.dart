71b2ef01647ff77f5c2a90ba4093db68c1db5b03
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-10-22 22:20:58 +0900

    feat: Add navigation helper and UI pages for authentication flow

--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:netflix/common/helper/navigation/app_navigation.dart';
 import 'package:netflix/core/configs/assets/app_images.dart';
+import 'package:netflix/presentation/auth/pages/signin.dart';
+import 'package:netflix/presentation/home/pages/home.dart';
+import 'package:netflix/presentation/splash/bloc/splash_cubit.dart';
+import 'package:netflix/presentation/splash/bloc/splash_state.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -7,29 +13,41 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                  AppImages.splashBackground,
+      body: BlocListener<SplashCubit, SplashState>(
+        listener: (context, state) {
+          if (state is UnAuthenticated) {
+            AppNavigation.pushReplacement(context, const SigninPage());
+          }
+
+          if (state is Authenticated) {
+            AppNavigation.pushReplacement(context, const HomePage());
+          }
+        },
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    AppImages.splashBackground,
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              gradient:LinearGradient(
-                begin: Alignment.center,
-                end: Alignment.bottomCenter,
-                colors: [
-                  const Color(0xff1A1B20).withOpacity(0),
-                  const Color(0xff1A1B20),
-              ],),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.center,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    const Color(0xff1A1B20).withOpacity(0),
+                    const Color(0xff1A1B20),
+                  ],
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
