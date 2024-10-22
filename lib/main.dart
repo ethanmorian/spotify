c2f34a2924ce61f4beb2e63f33bd26afab6a1661
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-10-22 22:17:58 +0900

    refactor: Update main.dart to initialize SplashCubit with BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:netflix/core/configs/theme/app_theme.dart';
+import 'package:netflix/presentation/splash/bloc/splash_cubit.dart';
 import 'package:netflix/presentation/splash/pages/splash.dart';
 
 void main() {
@@ -17,10 +19,13 @@ class MyApp extends StatelessWidget {
         statusBarColor: Colors.transparent,
       ),
     );
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.appTheme,
-      home: const SplashPage(),
+    return BlocProvider(
+      create: (context) => SplashCubit()..appStarted(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.appTheme,
+        home: const SplashPage(),
+      ),
     );
   }
 }
