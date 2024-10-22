41cffd6627b335216a01d276f2ea4cfe1e5bdd6a
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-10-22 18:52:12 +0900

    feat: implement splash screen layout

--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/core/configs/assets/app_images.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  AppImages.splashBackground,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              gradient:LinearGradient(
+                begin: Alignment.center,
+                end: Alignment.bottomCenter,
+                colors: [
+                  const Color(0xff1A1B20).withOpacity(0),
+                  const Color(0xff1A1B20),
+              ],),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
