7b3c3734bfb337d52d857b4374526a5a47f94499
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-10-24 19:11:15 +0900

    chore: Delete all files to start a new project

--- lib/service_locator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get_it/get_it.dart';
-import 'package:netflix/core/network/dio_client.dart';
-
-final sl = GetIt.instance;
-
-void setupServiceLocator() {
-  sl.registerSingleton<DioClient>(DioClient());
-}
