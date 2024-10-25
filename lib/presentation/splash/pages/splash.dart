28c62c8002f1f26a6c0d56388a061bd2bd339bf0
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-10-25 18:54:35 +0900

    feat(splash): add splash screen redirection to GetStartedPage after delay

--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify/core/config/assets/app_vectors.dart';
 
-class SplashPage extends StatelessWidget {
+import '../../intro/pages/get_started.dart';
+
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    redirect();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +25,20 @@ class SplashPage extends StatelessWidget {
         child: SvgPicture.asset(
           AppVectors.logo,
         ),
+      ),
+    );
+  }
+
+  Future<void> redirect() async {
+    await Future.delayed(
+      const Duration(
+        seconds: 2,
+      ),
+    );
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) => const GetStartedPage(),
       ),
     );
   }
