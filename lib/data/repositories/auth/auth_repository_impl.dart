18bd8f0f62478f9adb9bfdb4087bc40a05039417
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-10-30 19:53:56 +0900

    feat: add CreateUserReq model and AuthRepositoryImpl implementation

---
+++ lib/data/repositories/auth/auth_repository_impl.dart
@@ -0,0 +1,15 @@
+import 'package:spotify/domain/repositories/auth/auth.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  @override
+  Future<void> signin() {
+    // TODO: implement signin
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signup() {
+    // TODO: implement signup
+    throw UnimplementedError();
+  }
+}
