8e6f3c016c548bc2966282ee8e54d61a8c909c7f
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-10-31 19:44:04 +0900

    feat: enhance AuthRepository with Firebase integration for signu

--- lib/data/repositories/auth/auth_repository_impl.dart
+++ lib/data/repositories/auth/auth_repository_impl.dart
@@ -1,4 +1,8 @@
+import 'package:spotify/data/sources/auth/auth_firebase_service.dart';
 import 'package:spotify/domain/repositories/auth/auth.dart';
+
+import '../../../service_locator.dart';
+import '../../models/auth/create_user_req.dart';
 
 class AuthRepositoryImpl extends AuthRepository {
   @override
@@ -8,8 +12,7 @@ class AuthRepositoryImpl extends AuthRepository {
   }
 
   @override
-  Future<void> signup() {
-    // TODO: implement signup
-    throw UnimplementedError();
+  Future<void> signup(CreateUserReq createUserReq) async {
+    await sl<AuthFirebaseService>().signup(createUserReq);
   }
 }
