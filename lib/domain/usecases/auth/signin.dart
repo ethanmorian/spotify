1f5dbef5855273aab0ab3e31e863a176d4702e66
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-01 22:52:04 +0900

    feat: register SigninUseCase for dependency injection

---
+++ lib/domain/usecases/auth/signin.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:spotify/core/usecase/usecase.dart';
+import 'package:spotify/domain/repositories/auth/auth.dart';
+
+import '../../../data/models/auth/signin_user_req.dart';
+import '../../../service_locator.dart';
+
+class SigninUseCase implements UseCase<Either, SigninUserReq> {
+  @override
+  Future<Either> call({SigninUserReq? params}) async {
+    return sl<AuthRepository>().signin(params!);
+  }
+}
