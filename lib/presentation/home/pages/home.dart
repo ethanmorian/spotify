e05090465341beb4c41e3bd2848491e2754590c2
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-02 20:19:33 +0900

    feat: add logo and artist card layout on HomePage

--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify/core/config/assets/app_images.dart';
+
+import '../../../common/widgets/appbar/app_bar.dart';
+import '../../../core/config/assets/app_vectors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: BasicAppbar(
+        hideBack: true,
+        title: SvgPicture.asset(
+          AppVectors.logo,
+          height: 40,
+          width: 40,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _homeTopCard(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _homeTopCard() {
+    return Center(
+      child: SizedBox(
+        height: 140,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SvgPicture.asset(
+                AppVectors.homeTopCard,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  right: 60,
+                ),
+                child: Image.asset(
+                  AppImages.homeArtist,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
