d4bd55b0b3cc39871f87c7cd391c2fba03571c50
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-04 22:18:31 +0900

    feat: add app URLs constants for Firebase storage

---
+++ lib/core/config/constants/app_urls.dart
@@ -0,0 +1,5 @@
+class AppURLs {
+  static const firestorage = 'https://firebasestorage.googleapis.com/v0/b/spotify-7b750.firebasestorage.app/o/covers%2F';
+  static const midiaAlt = 'alt=media';
+
+}
