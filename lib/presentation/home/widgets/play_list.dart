0db30d423361d8703480526a6b272353d0917bd2
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-05 19:22:36 +0900

    feat: create PlayList widget to display songs with loading state handling

---
+++ lib/presentation/home/widgets/play_list.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify/common/helpers/is_dark_mode.dart';
+import 'package:spotify/domain/entities/song/song.dart';
+import 'package:spotify/presentation/home/bloc/play_list_cubit.dart';
+import 'package:spotify/presentation/home/bloc/play_list_state.dart';
+
+import '../../../core/config/theme/app_colors.dart';
+
+class PlayList extends StatelessWidget {
+  const PlayList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => PlayListCubit()..getPlayList(),
+      child: BlocBuilder<PlayListCubit, PlayListState>(
+        builder: (context, state) {
+          if (state is PlayListLoading) {
+            return Container(
+              alignment: Alignment.center,
+              child: const CircularProgressIndicator(),
+            );
+          }
+          if (state is PlayListLoaded) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 40,
+                horizontal: 16,
+              ),
+              child: Column(
+                children: [
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Playlist',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        'See More',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12,
+                          color: Color(0xffC6C6C6),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  _songs(state.songs),
+                ],
+              ),
+            );
+          }
+          return Container();
+        },
+      ),
+    );
+  }
+
+  Widget _songs(List<SongEntity> songs) {
+    return ListView.separated(
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 45,
+                  width: 45,
+                  transform: Matrix4.translationValues(10, 10, 0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: context.isDarkMode
+                        ? AppColors.dartGrey
+                        : const Color(0xffE6E6E6),
+                  ),
+                  child: Icon(
+                    Icons.play_arrow_rounded,
+                    color: context.isDarkMode
+                        ? const Color(0xff959595)
+                        : const Color(0xff555555),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      songs[index].title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      songs[index].artist,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 11,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  songs[index].duration.toString().replaceAll('.', ':'),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.favorite_rounded,
+                    color: AppColors.dartGrey,
+                  ),
+                )
+              ],
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        height: 20,
+      ),
+      itemCount: songs.length,
+    );
+  }
+}
