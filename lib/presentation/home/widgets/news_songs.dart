c5f192220479bb9ba328f652d7f71f7e5f746c51
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-06 16:25:04 +0900

    feat: navigate to SongPlayerPage on song click in NewsSongs

--- lib/presentation/home/widgets/news_songs.dart
+++ lib/presentation/home/widgets/news_songs.dart
@@ -5,6 +5,7 @@ import 'package:spotify/core/config/theme/app_colors.dart';
 import 'package:spotify/domain/entities/song/song.dart';
 import 'package:spotify/presentation/home/bloc/news_songs_cubit.dart';
 import 'package:spotify/presentation/home/bloc/news_songs_state.dart';
+import 'package:spotify/presentation/song_player/pages/song_player.dart';
 
 import '../../../core/config/constants/app_urls.dart';
 
@@ -39,65 +40,77 @@ class NewsSongs extends StatelessWidget {
     return ListView.separated(
       scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
-        return SizedBox(
-          width: 160,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage(
-                        '${AppURLs.firestorage}${songs[index].artist} - ${songs[index].title}.jpg?${AppURLs.midiaAlt}',
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SongPlayerPage(
+                  songEntity: songs[index],
+                ),
+              ),
+            );
+          },
+          child: SizedBox(
+            width: 160,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          '${AppURLs.firestorage}${songs[index].artist} - ${songs[index].title}.jpg?${AppURLs.midiaAlt}',
+                        ),
+                      ),
+                    ),
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        transform: Matrix4.translationValues(10, 10, 0),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: context.isDarkMode
+                              ? AppColors.dartGrey
+                              : const Color(0xffE6E6E6),
+                        ),
+                        child: Icon(
+                          Icons.play_arrow_rounded,
+                          color: context.isDarkMode
+                              ? const Color(0xff959595)
+                              : const Color(0xff555555),
+                        ),
                       ),
                     ),
                   ),
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: Container(
-                      height: 40,
-                      width: 40,
-                      transform: Matrix4.translationValues(10, 10, 0),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: context.isDarkMode
-                            ? AppColors.dartGrey
-                            : const Color(0xffE6E6E6),
-                      ),
-                      child: Icon(
-                        Icons.play_arrow_rounded,
-                        color: context.isDarkMode
-                            ? const Color(0xff959595)
-                            : const Color(0xff555555),
-                      ),
-                    ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  songs[index].title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                songs[index].title,
-                style: const TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16,
+                const SizedBox(
+                  height: 5,
                 ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Text(
-                songs[index].artist,
-                style: const TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 12,
+                Text(
+                  songs[index].artist,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
