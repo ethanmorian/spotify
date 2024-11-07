02a019d7e63e2817116e0fc5ea663edaa7116aff
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-07 21:42:15 +0900

    feat: implement AddOrRemoveFavoriteSongUseCase for managing favorite songs

---
+++ lib/domain/usecases/song/add_or_remove_favorite_song.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:spotify/core/usecase/usecase.dart';
+import 'package:spotify/domain/repositories/song/song.dart';
+
+import '../../../service_locator.dart';
+
+class AddOrRemoveFavoriteSongUseCase implements UseCase<Either, String> {
+  @override
+  Future<Either> call({String? params}) async {
+    return await sl<SongRepository>().addOrRemoveFavoriteSongs(params!);
+  }
+}
