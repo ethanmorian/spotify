72750a648faa40c6b26ad2002f7ca9aa888c522d
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-09 16:04:46 +0900

    feat: Integrate ProfileInfoCubit into ProfilePage for dynamic user profile UI

--- lib/presentation/profile/pages/profile.dart
+++ lib/presentation/profile/pages/profile.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spotify/common/helpers/is_dark_mode.dart';
 import 'package:spotify/common/widgets/appbar/app_bar.dart';
-import 'package:spotify/core/config/theme/app_colors.dart';
+import 'package:spotify/presentation/profile/bloc/profile_info_cubit.dart';
+import 'package:spotify/presentation/profile/bloc/profile_info_state.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -24,13 +26,71 @@ class ProfilePage extends StatelessWidget {
   }
 
   Widget _profileInfo(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height / 3.5,
-      decoration: BoxDecoration(
-        color: context.isDarkMode ? const Color(0xff2C2B2B) : Colors.white,
-        borderRadius: const BorderRadius.only(
-          bottomRight: Radius.circular(30),
-          bottomLeft: Radius.circular(30),
+    return BlocProvider(
+      create: (context) => ProfileInfoCubit()..getUser(),
+      child: Container(
+        height: MediaQuery.of(context).size.height / 3.5,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: context.isDarkMode ? const Color(0xff2C2B2B) : Colors.white,
+          borderRadius: const BorderRadius.only(
+            bottomRight: Radius.circular(50),
+            bottomLeft: Radius.circular(50),
+          ),
+        ),
+        child: BlocBuilder<ProfileInfoCubit, ProfileInfoState>(
+          builder: (context, state) {
+            if (state is ProfileInfoLoading) {
+              return Container(
+                alignment: Alignment.center,
+                child: const CircularProgressIndicator(),
+              );
+            }
+
+            if (state is ProfileInfoLoaded) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 90,
+                    width: 90,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          state.userEntity.imageURL!,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    state.userEntity.email!,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    state.userEntity.fullName!,
+                    style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              );
+            }
+
+            if (state is ProfileInfoFailure) {
+              return const Text(
+                'Please try again',
+              );
+            }
+
+            return Container();
+          },
         ),
       ),
     );
