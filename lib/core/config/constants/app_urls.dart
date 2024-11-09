439a36b8ade0289178973629d847b97f4e7c0886
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-09 15:39:46 +0900

    feat: add default image URL to AppURLs

--- lib/core/config/constants/app_urls.dart
+++ lib/core/config/constants/app_urls.dart
@@ -4,4 +4,5 @@ class AppURLs {
   static const songFirestorage =
       'https://firebasestorage.googleapis.com/v0/b/spotify-7b750.firebasestorage.app/o/songs%2F';
   static const midiaAlt = 'alt=media';
+  static const defaultImage = 'https://cdn-icons-png.flaticon.com/512/10542/10542486.png';
 }
