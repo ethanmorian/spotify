8f1a4fe591b2aa2cefa0b36c179765e29d88040b
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-10 19:05:57 +0900

    fix: update mediaAlt typo to correct spelling

--- lib/core/config/constants/app_urls.dart
+++ lib/core/config/constants/app_urls.dart
@@ -3,6 +3,6 @@ class AppURLs {
       'https://firebasestorage.googleapis.com/v0/b/spotify-7b750.firebasestorage.app/o/covers%2F';
   static const songFirestorage =
       'https://firebasestorage.googleapis.com/v0/b/spotify-7b750.firebasestorage.app/o/songs%2F';
-  static const midiaAlt = 'alt=media';
+  static const mediaAlt = 'alt=media';
   static const defaultImage = 'https://cdn-icons-png.flaticon.com/512/10542/10542486.png';
 }
